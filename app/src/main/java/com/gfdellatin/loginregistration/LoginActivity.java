60c69a4dd49072a42abdf6c44c62e3990f9aaccf
GuilhermeDellatin <guidellatin@gmail.com>
2020-12-23 16:50:37 -0300

    Tela de Login concluida

--- app/src/main/java/com/gfdellatin/loginregistration/LoginActivity.java
+++ app/src/main/java/com/gfdellatin/loginregistration/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.gfdellatin.loginregistration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText userEmail, userPassword;
     private Button buttonLogin;
     private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+    private ImageView loginPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +34,82 @@ public class LoginActivity extends AppCompatActivity {
 
         userEmail = findViewById(R.id.register_email);
         userPassword = findViewById(R.id.register_password);
-
         buttonLogin = findViewById(R.id.loginBtn);
+        loginProgress = findViewById(R.id.login_progress);
+        loginPhoto = findViewById(R.id.login_photo);
+        mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, HomeActivity.class);
+
+        loginProgress.setVisibility(View.INVISIBLE);
+
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
+        
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginProgress.setVisibility(View.VISIBLE);
+                buttonLogin.setVisibility(View.INVISIBLE);
+                
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                
+                if (email.isEmpty() || password.isEmpty()) {
+                    showMessage("Por favor verifique todos os campos");
+                    buttonLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    signIn(email, password);
+                }
+            }
+        });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    buttonLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                    buttonLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            //user is already connected so we need to redirect him to home page
+            updateUI();
+        }
     }
 }
